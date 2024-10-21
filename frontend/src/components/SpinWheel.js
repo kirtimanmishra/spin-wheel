c84f7c189e74a3572f23d8f2769b2c208179e1f1
kirtimanmishra <mishrakirtiman24@gmail.com>
2024-10-21 15:14:40 +0530

    UI

--- frontend/src/components/SpinWheel.js
+++ frontend/src/components/SpinWheel.js
@@ -1,19 +1,19 @@
 import React, { useState, useEffect } from "react";
 import Wheel from "./Wheel";
-import trumpImage from "../images/trump.png"; // Import Trump image
-import styles from "./SpinWheel.module.css"; // Import the CSS module
-import initImage1 from "../images/init_image_1.png"; // Import Biden image
-import initImage2 from "../images/init_image_2.png"; // Import Biden image
-import initImage3 from "../images/init_image_3.png"; // Import Biden image
-import kamalaImage from "../images/kamala.png"; // Import Biden image
+import trumpImage from "../images/trump.png";
+import styles from "./SpinWheel.module.css";
+import initImage1 from "../images/init_image_1.png";
+import initImage2 from "../images/init_image_2.png";
+import initImage3 from "../images/init_image_3.png";
+import kamalaImage from "../images/kamala.png";
 import GlobalVotes from "./GlobalVotes";
 import UserVotes from "./UserVotes";
 
 const SpinWheel = () => {
-  const candidates = [trumpImage, kamalaImage]; // Use images instead of names
-  const [slots, setSlots] = useState([initImage1, initImage2, initImage3]); // Initial slots
+  const candidates = [trumpImage, kamalaImage];
+  const [slots, setSlots] = useState([initImage1, initImage2, initImage3]);
   const [spinning, setSpinning] = useState(false);
-  const [winner, setWinner] = useState(""); // State to track the winner
+  const [winner, setWinner] = useState("");
   const [toggleWinner, setToggleWinner] = useState(false);
 
   const images = [kamalaImage, trumpImage, kamalaImage, trumpImage];
@@ -23,23 +23,19 @@ const SpinWheel = () => {
     images[2],
   ]);
 
-  // Function to spin the wheel and determine the winner
   const spin = () => {
     setSpinning(true);
 
     setTimeout(() => {
-      // Randomly assign Trump or Biden images to each slot
       const newSlots = slots.map(
         () => candidates[Math.floor(Math.random() * 2)]
       );
       setSlots(newSlots);
-      // calculateWinner(newSlots);
       const trumpCount = newSlots.filter((slot) => slot === trumpImage).length;
       const kamalaCount = newSlots.filter(
         (slot) => slot === kamalaImage
       ).length;
 
-      // Determine the winner
       let newWinner = "No winner";
       if (trumpCount >= 2) {
         newWinner = "trump";
@@ -49,30 +45,8 @@ const SpinWheel = () => {
       setWinner(newWinner);
       setToggleWinner((prev) => !prev);
       setSpinning(false);
-    }, 2000); // Spin for 2 seconds
+    }, 1000);
   };
-
-  // const calculateWinner = (newSlots) => {
-  //   // Count occurrences of Trump and Biden
-  //   setTimeout(() => {
-  //     const trumpCount = newSlots.filter((slot) => slot === trumpImage).length;
-  //     const kamalaCount = newSlots.filter(
-  //       (slot) => slot === kamalaImage
-  //     ).length;
-
-  //     // Determine the winner
-  //     let newWinner = "No winner";
-  //     if (trumpCount >= 2) {
-  //       newWinner = "Trump";
-  //     } else if (kamalaCount >= 2) {
-  //       newWinner = "Kamala";
-  //     }
-  //     setWinner(newWinner);
-  //     setToggleWinner((prev) => !prev);
-  //     setSpinning(false);
-  //     setToggleWinner((prev) => !prev);
-  //   }, 4000);
-  // };
 
   useEffect(() => {
     if (spinning) {
@@ -82,18 +56,16 @@ const SpinWheel = () => {
             () => images[Math.floor(Math.random() * images.length)]
           )
         );
-      }, 200); // Change images every 100ms
+      }, 200);
 
       return () => clearInterval(intervalId);
     } else {
-      // Reset the images when not spinning
       setCurrentImages([images[0], images[1], images[2]]);
     }
   }, [spinning]);
 
   return (
     <div className={styles.spinWheelContainer}>
-      {/* Conditional rendering based on spinning state */}
       {spinning ? (
         <div className={styles.loader}>
           {currentImages.map((img, index) => (
