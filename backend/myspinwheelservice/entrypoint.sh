1aad8edb0c6bb596f90ef50b8385f3eb12234cbc
kirtimanmishra <mishrakirtiman24@gmail.com>
2024-10-23 15:17:36 +0530

    BE deployed on cloud run

--- backend/myspinwheelservice/entrypoint.sh
+++ backend/myspinwheelservice/entrypoint.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-
+echo "Activating virtual environment..."
 . .venv/bin/activate
 
+echo "Running makemigrations..."
 pdm run makemigrations
+
+echo "Running migrate..."
 pdm run migrate
 
+echo "Starting Gunicorn..."
 .venv/bin/gunicorn \
 --bind 0.0.0.0:8000 \
 --workers 2 \
