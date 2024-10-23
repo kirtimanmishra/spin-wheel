15335fa9fdcfb52dec600e37213b71a4c52ba3cc
kirtimanmishra <mishrakirtiman24@gmail.com>
2024-10-23 21:20:15 +0530

    fix

--- backend/myspinwheelservice/entrypoint.sh
+++ backend/myspinwheelservice/entrypoint.sh
@@ -11,6 +11,6 @@ pdm run migrate
 echo "Starting Gunicorn..."
 .venv/bin/gunicorn \
 --bind 0.0.0.0:8000 \
---workers 2 \
---threads 1 \
+--workers 5 \
+--threads 2 \
 myspinwheelservice.wsgi:application
